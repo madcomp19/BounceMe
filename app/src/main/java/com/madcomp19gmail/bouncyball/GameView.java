54c14570ff0ed4a65b31095ac4241c6e4c43851b
dcx2202 <31130828+dcx2202@users.noreply.github.com>
2019-03-05 00:01:39 +0000

    Removed test background

--- app/src/main/java/com/madcomp19gmail/bouncyball/GameView.java
+++ app/src/main/java/com/madcomp19gmail/bouncyball/GameView.java
@@ -11,6 +11,7 @@ import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.drawable.BitmapDrawable;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import java.util.ArrayList;
@@ -46,7 +47,7 @@ public class GameView extends View
 
         // This crops the image to the screen size starting from the bottom left corner
         Resources resources = getResources();
-        Bitmap bmp = BitmapFactory.decodeResource(resources, R.drawable.test_background);
+        Bitmap bmp = BitmapFactory.decodeResource(resources, R.drawable.background_42);
         BitmapDrawable background = new BitmapDrawable(resources, Bitmap.createBitmap(bmp, 0,bmp.getHeight() - height, width, height));
         setBackgroundDrawable(background);
         bmp.recycle();
