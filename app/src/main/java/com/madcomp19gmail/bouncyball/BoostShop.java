bbccd7d7258c0de1870299180a575393b44d376a
diogon6 <32933699+diogon6@users.noreply.github.com>
2019-03-23 13:41:12 +0000

    Boost shop prices working

--- app/src/main/java/com/madcomp19gmail/bouncyball/BoostShop.java
+++ app/src/main/java/com/madcomp19gmail/bouncyball/BoostShop.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
@@ -18,6 +20,8 @@ public class BoostShop extends AppCompatActivity {
 
     private AdView mAdView;
     StorageManager storageManager;
+    TextView coins;
+    TextView gems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +32,11 @@ public class BoostShop extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         storageManager = StorageManager.getInstance();
+        coins = findViewById(R.id.coins);
+        gems = findViewById(R.id.gems);
+
+        coins.setText(storageManager.getTotalTouches() + "");
+        gems.setText(storageManager.getTotalGems() + "");
 
         mAdView = findViewById(R.id.bannerAdBoostShop);
         AdRequest adRequest = new AdRequest.Builder().build();
@@ -48,7 +57,27 @@ public class BoostShop extends AppCompatActivity {
         int boost = parseInt(view_id.split("_")[0].replace("x", ""));
         int time = parseInt(view_id.split("_")[1].replace("m", ""));
 
-        storageManager.setActiveBoost(boost);
-        storageManager.setActiveBoostTime(Calendar.getInstance().getTimeInMillis() + time * 60000);
+        String label = view_id + "_Label";
+        int id = getResources().getIdentifier(label, "id", getApplicationContext().getPackageName());
+        TextView label_text = findViewById(id);
+        if(label_text.getText() == "")
+            return;
+        int price = Integer.parseInt(label_text.getText() + "");
+        int total_gems = storageManager.getTotalGems();
+        if(total_gems > price)
+        {
+            storageManager.takeGems(price);
+            gems.setText(storageManager.getTotalGems() + "");
+            storageManager.setActiveBoost(boost);
+            storageManager.setActiveBoostTime(Calendar.getInstance().getTimeInMillis() + time * 60000);
+            Toast.makeText(this, "Boost is now active", Toast.LENGTH_SHORT).show();
+            label_text.setText("");
+            label_text.setCompoundDrawablesWithIntrinsicBounds( 0, R.drawable.selected_icon_vector, 0, 0);
+            label_text.setPadding(0,10,0,0);
+        }
+        else
+            Toast.makeText(this, "You need " + (price - total_gems) + " more Gems!", Toast.LENGTH_LONG).show();
+
+
     }
 }
